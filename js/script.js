8a6d07f4c29c08c37a8660aee9e153b990c31841
stique93 <18rio@mail.ru>
2019-04-26 13:50:30 +0300

    Add js and css files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+if (location.hash.indexOf("#access_token=") === 0) {
+    localStorage.token = location.hash.substring(14, 99);
+    localStorage.session = Date.now() + 86400;
+    location.hash = "";
+    console.log(location.hash);
+  }
