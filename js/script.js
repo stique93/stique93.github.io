904125198c604f586c070ff1c901c03dbece4c91
unknown <18rio@mail.ru>
2019-04-26 23:48:32 +0300

    Code refactoring

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ if (location.hash.indexOf("#access_token=") === 0) {
     location.hash = "";
 }
 
-if(localStorage.token){
+if (localStorage.token) {
     loadFriends();
     $("#guest").hide();
     $("#btnAuth").hide();
+    $("#msg").hide();
 }
 
 
@@ -29,12 +30,12 @@ function sendRequest(method, params, func) {
 
 function loadFriends() {
     //var search = window.document.getElementById('search').value;
-    sendRequest('users.get', {fields: 'photo_200'},function (data) {
-        $('#user').html(data.response[0].first_name + " " + data.response[0].last_name);
-        $('#btnExit').html(' '+'<button type="button" class="btn btn-secondary" onclick="localStorage.clear(); location.reload();">Выйти</button>');
+    sendRequest('users.get', { fields: 'photo_100' }, function (data) {
+        $('#user').html(data.response[0].first_name + " " + data.response[0].last_name + "<img src ='" + friend.photo_100 + "'></img>");
+        $('#btnExit').html(' ' + '<button type="button" class="btn btn-secondary" onclick="localStorage.clear(); location.reload();">Выйти</button>');
         $("#user")[0].hidden = false;
     });
-    sendRequest('friends.search', { count: 5, fields: 'photo_100,online,sex,bdate'}, function (data) {
+    sendRequest('friends.search', { count: 5, fields: 'photo_100,online,sex,bdate' }, function (data) {
         friendsList = data.response.items;
         drawFriends(friendsList);
     });
